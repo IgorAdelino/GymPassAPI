0a853c06cfb87966651fb4200de366490ae2f5f5
Igor Adelino <igoradelino@gmail.com>
2023-08-09 00:09:24 -0300

    First commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
+
+export const app = fastify()
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // here we should log to an external tool like Datagog/NewRelic
+    // ou seja são ferramentas de observabilidade
+  }
+
+  return reply.status(500).send()
+})
---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,22 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UsersRepository } from '../user-repository'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const User = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    return User
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+}
