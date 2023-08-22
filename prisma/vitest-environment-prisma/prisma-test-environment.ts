0faed6e89785d47a254fee229c59a7a3449a6dff
Igor Adelino <igoradelino@gmail.com>
2023-08-22 01:43:18 -0300

    Iniciando os testes E2E

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,13 @@
+import { Environment } from 'vitest'
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    console.log('Setup')
+    return {
+      teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+}
