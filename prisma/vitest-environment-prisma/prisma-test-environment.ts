920e8a4555eae7e5fba94632d257047213fe45df
Igor Adelino <igoradelino@gmail.com>
2023-08-23 00:08:49 -0300

    Criando o test environment para os e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,39 @@
-import { Environment } from 'vitest'
+import 'dotenv/config'
 
+import { randomUUID } from 'node:crypto'
+import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+// "postgresql://docker:docker@localhost:5432/apisolid?schema=public"
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL enviroment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const dataBaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = dataBaseURL
+    execSync('npx prisma migrate deploy') // o deploy apenas executa cada um dos arquivos sem verificação
     return {
-      teardown() {
-        console.log('Teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
